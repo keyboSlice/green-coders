fe32268c48066d216f9933067a443ec2df4916f4
Keir Lavelle <keirlavelle1@hotmail.co.uk>
2018-10-27 16:53:08 +0100

    adding intro page

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,50 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center>
-    <v-flex
-      xs12
-      sm8
-      md6>
-      <div class="text-xs-center">
-        <logo/>
-        <vuetify-logo/>
-      </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a
-            href="https://vuetifyjs.com"
-            target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a
-            href="https://chat.vuetifyjs.com/"
-            target="_blank"
-            title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a
-            href="https://github.com/vuetifyjs/vuetify/issues"
-            target="_blank"
-            title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank">Nuxt Documentation</a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank">Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container class="app-background">
+    <v-card class="app-intro">
+      <h1><strong>PAPP</strong><br>The passive aggressive potted plant</h1>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {
-    components: {}
-}
+export default {}
 </script>
+
+<style lang="scss">
+.app-background {
+    background-image: url('/papp.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+
+    &::after {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 1;
+        background: #fff;
+        content: '';
+        opacity: 0.25;
+    }
+
+    > * {
+        z-index: 2;
+    }
+}
+
+.app-intro {
+    font-family: 'Amatic SC', cursive;
+    text-align: center;
+    font-weight: normal !important;
+
+    strong {
+        font-weight: bold;
+    }
+}
+</style>
